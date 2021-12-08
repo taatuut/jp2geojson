8032701e334b0aa7f8a28bbac7c9a216949ef77b
Emil Zegers <e.zegers@upcmail.nl>
2021-12-08 12:10:53 +0100

    Initial working commit.

---
+++ exme.py
@@ -0,0 +1,32 @@
+# exme.py
+
+# python3 exme.py 
+
+import glymur
+import json
+import os
+
+def myconverter(o):
+    return o.__str__()
+
+def jp2Metadata(jp2file=None, options=None):
+    jp2file = glymur.data.nemo() if jp2file is None else jp2file
+    path = "./Output/"
+    jp2name = jp2file.split('/')[-1]
+    jp2 = glymur.Jp2k(jp2file)
+    textfile = os.path.join(path,jp2name+".txt")
+    jsonfile = os.path.join(path,jp2name+".json")
+    with open(textfile, 'w+') as tf:
+        tf.write(str(jp2))
+    dict1 = {}
+    with open(textfile) as fh:  
+        for line in fh:
+            list = line.strip().split(':')
+            key = list[0]
+            val = ""
+            if len(list) > 1:
+                val = list[1:]
+            dict1[key] = " ".join(val).strip()
+    with open(jsonfile, 'w') as jf:
+        #jf.write(json.dump(dict1, jsonfile, indent = 2, sort_keys = False))
+        jf.write(json.dumps(dict1, default = myconverter))
---
+++ test_exme.py
@@ -0,0 +1,21 @@
+# test_exme.py
+
+# python3 test_exme.py
+
+# python3 -m pip install glymur
+# ls /usr/local/lib/python3.9/site-packages/glymur/data/
+# goodstuff.j2k   heliov.jpx      nemo.jp2
+# cp /usr/local/lib/python3.9/site-packages/glymur/data/*.j* ./JPEG2000/
+
+# Tested on MacOS with Python 3.9 and PyPy 3.8 with glymur installed, borrowed the sample files from glymur
+
+import exme
+
+for i in ['./JPEG2000/goodstuff.j2k',
+            './JPEG2000//heliov.jpx',
+            './JPEG2000/nemo.jp2',
+            './JPEG2000/sample1.jp2']:
+    exme.jp2Metadata(i)
+
+# Expected result:
+# For every input file a .txt and .json file in ./Output with either raw jp2 data and converted to json
