34ecf1334a23a2beedc73a89973d38c630883459
Emil Zegers <e.zegers@upcmail.nl>
2021-12-13 02:05:44 +0100

    Cleanup comments in test_main.py. Minor change (removing newline) in README.md.

--- test_main.py
+++ test_main.py
@@ -7,23 +7,19 @@
 # goodstuff.j2k   heliov.jpx      nemo.jp2
 # cp /usr/local/lib/python3.9/site-packages/glymur/data/*.j* ./JPEG2000/
 
-# Tested on MacOS with Python 3.9 and PyPy 3.8 with glymur and pyyaml installed, borrowed the sample files from glymur
+# Tested on MacOS with Python 3.9 and PyPy 3.8 with glymur and pyyaml installed
+
+#  borrowed the sample files from glymur
+
+# python3 test_main.py JPEG2000/ -o ./Results/geojson.json -l ./Results/map.html
 
 import main
-
-# TODO: change to using dir and output arguments like in photos2geojson main.py
-
-#j = ['./JPEG2000//heliov.jpx']
-#j = ['./JPEG2000/goodstuff.j2k','./JPEG2000//heliov.jpx','./JPEG2000/nemo.jp2','./JPEG2000/sample1.jp2']
-#for i in j:
-    #exme.jp2Metadata(i)
 
 main.main()
 
 # Expected result:
-# For every input file a .txt and .json file in ./Output with either raw jp2 data and converted to json
+# In the output folder (use ./Results in this repo):
+# - For every input file a .txt with raw JPEG 2000 data
+# - and one (1) .json file for all images containing the metadata converted to geojson
 
-
-# python3 test_main.py JPEG2000/ -o ./Results/geojson.json -l ./Results/map.html
-
-# TODO: does it also write map.html if -l not provided? check!
+# Then pickup with mongoimport or similar to ingest into MongoDB (Atlas)
